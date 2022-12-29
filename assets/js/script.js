ddf05e9c95d5202ba98a01ab6e5db4bb64c925a4
illusiveZ <106180412+illusiveZ@users.noreply.github.com>
2022-12-29 14:47:24 +0000

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let navigation = document.querySelector(".navigation");
+
+let close = document.querySelector(".close");
+
+navigation.onclick = function () {
+  navigation.classList.add("active");
+};
+
+close.onclick = function () {
+  navigation.classList.remove("active");
+};
